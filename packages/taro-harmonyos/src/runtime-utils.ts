f1857b51b5e19d3cfcc9bb39e42cc5c85d501877
chenjiajian <798095202@qq.com>
2021-08-09 10:00:54 +0800

    feat(harmonyos): 增加鸿蒙插件

---
+++ packages/taro-harmonyos/src/runtime-utils.ts
@@ -0,0 +1,21 @@
+import { internalComponents } from '@tarojs/shared'
+import { initNativeApi } from './apis'
+
+export { initNativeApi }
+export * from './components'
+export const hostConfig = {
+  initNativeApi,
+  modifyDispatchEvent (event, tagName) {
+    const comps = Object.keys(internalComponents).map(c => c.toLowerCase())
+    tagName = tagName.toLowerCase()
+    if (event.type === 'click' && comps.includes(tagName)) {
+      event.type = 'tap'
+    }
+  },
+  getPathIndex (indexOfNode) {
+    return indexOfNode
+  },
+  getSpecialNodes () {
+    return []
+  }
+}
---
+++ packages/taro-harmonyos/types/runtime-utils.d.ts
@@ -0,0 +1,8 @@
+import { initNativeApi } from './apis';
+export { initNativeApi };
+export * from './components';
+export declare const hostConfig: {
+    initNativeApi: typeof initNativeApi;
+    modifyDispatchEvent(event: any, tagName: any): void;
+    getPathIndex(indexOfNode: any): any;
+};
